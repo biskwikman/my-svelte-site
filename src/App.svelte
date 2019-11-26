48bb296a3b99bc76451f19f85ee0279972b2f619
biskwikman <djosephhenri@gmail.com>
2019-11-26 16:51:16 -0500

    first workableversion

--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,17 @@
 
 	function clickMaps() {
 		clickedButton = 'maps';
+		document.getElementById('maps-button').style.backgroundColor = '#526488';
+		document.getElementById('maps-button').style.color = 'white';
+		document.getElementById('writing-button').style.backgroundColor = 'white';
+		document.getElementById('writing-button').style.color = '#526488';
 	}
 	function clickWriting()	{
 		clickedButton = 'writing';
+		document.getElementById('maps-button').style.backgroundColor = 'white';
+		document.getElementById('maps-button').style.color = '#526488';
+		document.getElementById('writing-button').style.backgroundColor = '#526488';
+		document.getElementById('writing-button').style.color = 'white';
 	}
 </script>
 
@@ -24,17 +32,27 @@
 	}
 
 	button {
-		border: none;
+		border: 2px solid gray;
 		background: none;
+		border-radius: 5px;
+		box-shadow: 2px 2px 10px #526488;
 		cursor: pointer;
+		width: calc(4em + 1vw);
+	}
+
+	button:active {
+		position: relative;
+		top: 5px;
+		left: 5px;
+		box-shadow: 0px 0px 15px #526488;
 	}
 </style>
 <Header/>
 
 <div>
 <div class="main-buttons">
-	<button class="title" on:click={clickMaps}>Maps <br> 地図</button>
-	<button class="title" on:click={clickWriting}>Writing <br> 文章</button>
+	<button class="title" id="maps-button" on:click={clickMaps}>Maps <br> 地図</button>
+	<button class="title" id="writing-button" on:click={clickWriting}>Writing <br> 文章</button>
 </div>
 {#if clickedButton == 'maps'}
 	<Maps/>
--- src/components/Writing.svelte
+++ src/components/Writing.svelte
@@ -33,43 +33,43 @@
     <div class="item">
         <a href="https://overland.org.au/2019/11/the-myth-of-the-most-successful-multicultural-country-in-the-world/">
             <h1 class="title">The Myth of 'The Most Successful Multicultural Country in the World'</h1>
-            <p>Overland Journal - 2019.11.07</p>
+            <p>Overland Journal - 2019.11.07 --- politics・政治</p>
         </a>
     </div>
     <div class="item">
         <a href="https://qiita.com/biskwikman/items/b09de2b470e1dabcaed2">
             <h1 class="title">pythonで東京の人口密度のちずを使いましょう！</h1>
-            <p>Qiita - 2018.11.27</p>
+            <p>Qiita - 2018.11.27 --- tech・技術</p>
         </a>
     </div>
     <div class="item">
         <a href="https://qiita.com/biskwikman/items/c75d24d7bef701e04137">
             <h1 class="title">JupyterlabでRとJavascriptを使おう</h1>
-            <p>Qiita - 2018.10.13</p>
+            <p>Qiita - 2018.10.13 --- tech・技術</p>
         </a>
     </div>
     <div class="item">
         <a href="https://medium.com/@djosephhenri/mapping-detroits-high-poverty-areas-and-their-access-to-public-transit-using-geopandas-mapbox-72fa21a0bd79">
             <h1 class="title">Mapping Detroit's High Poverty Areas</h1>
-            <p>Medium - 2018.5.17</p>
+            <p>Medium - 2018.5.17 --- tech・技術</p>
         </a>
     </div>
     <div class="item">
         <a href="https://medium.com/@djosephhenri/creating-a-population-density-map-of-toyota-city-japan-using-d3-geos-cli-and-some-other-stuff-4bbfb73f37e5">
             <h1 class="title">Creating a Population Density Map of Toyota City, Japan - Pt 3</h1>
-            <p>Medium - 2018.14.03</p>
+            <p>Medium - 2018.14.03 --- tech・技術</p>
         </a>
     </div>
     <div class="item">
         <a href="https://medium.com/@djosephhenri/creating-a-population-density-map-of-toyota-city-japan-using-d3-geos-cli-and-some-other-stuff-396c5b6dc225">
             <h1 class="title">Creating a Population Density Map of Toyota City, Japan - Pt 2</h1>
-            <p>Medium - 2018.13.03</p>
+            <p>Medium - 2018.13.03 --- tech・技術</p>
         </a>
     </div>
     <div class="item">
         <a href="https://medium.com/@djosephhenri/creating-a-population-density-map-of-toyota-city-japan-using-d3-geos-cli-and-some-other-stuff-705632a55e8f">
             <h1 class="title">Creating a Population Density Map of Toyota City, Japan - Pt 1</h1>
-            <p>Medium - 2018.03.03</p>
+            <p>Medium - 2018.03.03 --- tech・技術</p>
         </a>
     </div>
 </div>
