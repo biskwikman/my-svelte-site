b7c90fb93179e4f2f502917d0b2873e347d199c2
Daniel Henri <Daniel Henri>
2021-10-26 17:30:32 +0900

    removed p-arts

--- src/components/Writing.svelte
+++ src/components/Writing.svelte
@@ -41,13 +41,13 @@
             <h1 class="title">Australian Media's Obsession with "Overpopulation"</h1>
             <p>Mag Mag - 2019.12.29 --- politics・政治</p>
         </a>
-    </div> -->
+    </div>
     <div class="item">
         <a href="https://overland.org.au/2019/11/the-myth-of-the-most-successful-multicultural-country-in-the-world/">
             <h1 class="title">The Myth of 'The Most Successful Multicultural Country in the World'</h1>
             <p>Overland Journal - 2019.11.07 --- politics・政治</p>
         </a>
-    </div>
+    </div> -->
     <div class="item">
         <a href="https://qiita.com/biskwikman/items/b09de2b470e1dabcaed2">
             <h1 class="title">pythonで東京の人口密度のちずを使いましょう！</h1>
